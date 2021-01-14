9f927adfced064590c8d4f4ace59501ef1b18334
Khwilo Kabaka <khwilowatai@gmail.com>
2021-01-14 16:23:15 +0300

    Add method to fetch a single candidate

--- src/api/index.js
+++ src/api/index.js
@@ -7,3 +7,13 @@ export const fetchCandidates = async () => {
     return error;
   }
 };
+
+export const fetchCandidate = async (id) => {
+  try {
+    const response = await fetch(`http://localhost:4000/candidates/${id}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
+};
