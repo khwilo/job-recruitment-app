aaaff9729cc2f6804553f17e9f2fabc74d003d11
Khwilo Kabaka <khwilowatai@gmail.com>
2021-01-14 17:34:31 +0300

    Create and style the Profile component

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,18 +1,23 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
-import { fetchCandidate } from '../../api';
+import { fetchCandidate, fetchCandidateApplication } from '../../api';
 import './profile.css';
 
 const Profile = () => {
   const [candidate, setCandidate] = React.useState({});
+  const [applications, setApplications] = React.useState([]);
   const { id: candidateId } = useParams();
 
   React.useEffect(() => {
     fetchCandidate(candidateId).then((data) => {
       setCandidate(data);
     });
-  }, [candidateId]);
+
+    fetchCandidateApplication(candidateId).then((data) => {
+      setApplications(data);
+    });
+  }, []);
 
   return (
     <div className='profile wrapper'>
@@ -29,6 +34,43 @@ const Profile = () => {
           {candidate.yearsOfExperience}
         </p>
       </section>
+      <div className='profile__footer'>
+        {applications.length === 0 ? (
+          <div className='d-flex warning'>
+            <img src='/images/warning.svg' alt='user avatar' />
+            <p className='text-bold ml-text'>
+              {candidate.name} has no application yet!
+            </p>
+          </div>
+        ) : (
+          <div className='d-flex'>
+            <img src='/images/check.svg' alt='check icon' />
+            <p className='ml-text text-bold'>
+              {candidate.name} has job applications available
+            </p>
+          </div>
+        )}
+        {applications.map((application, index) => (
+          <div key={index}>
+            {Object.keys(application.resume).length === 0 &&
+            application.resume.constructor === Object ? (
+              <div className='d-flex'>
+                <img src='/images/cancel.svg' alt='check icon' />
+                <p className='ml-text text-bold'>No resume attached</p>
+              </div>
+            ) : (
+              <div className='d-flex'>
+                <img src='/images/attachment.svg' alt='check icon' />
+                <p className='ml-text'>
+                  <Link className='viewCV-text' to={`/resume/${candidate.id}`}>
+                    View CV
+                  </Link>
+                </p>
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
