b336c9b95c4755b22680be6f4228e3abdae446be
Khwilo Kabaka <khwilowatai@gmail.com>
2021-01-14 17:33:35 +0300

    Fetch a candidate's applications

--- src/api/index.js
+++ src/api/index.js
@@ -17,3 +17,15 @@ export const fetchCandidate = async (id) => {
     return error;
   }
 };
+
+export const fetchCandidateApplication = async (id) => {
+  try {
+    const response = await fetch(
+      `http://localhost:4000/candidates/${id}/applications`
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
+};
