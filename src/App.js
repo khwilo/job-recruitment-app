19ec54f0c3efaf33e96c27bcc70e09a9281b235c
Khwilo Kabaka <khwilowatai@gmail.com>
2021-01-14 13:18:36 +0300

    Refactor candidates data in components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import './App.css';
 import Nav from './components/Nav';
+import CandidatesList from './containers/Candidates';
 
 function App() {
   const [candidates, setCandidates] = React.useState([]); // save candidates data here
@@ -28,20 +29,7 @@ function App() {
     <div>
       <Nav />
 
-      {/* Iterate through the candidates data and display them on the screen */}
-      {candidates.map((candidate) => {
-        return (
-          <div key={candidate.id}>
-            <p>
-              {candidate.name.first} {candidate.name.last}
-            </p>
-            <p>{candidate.yearsOfExperience}</p>
-            <p>{candidate.jobTitle}</p>
-            <p>{candidate.email}</p>
-            <hr />
-          </div>
-        );
-      })}
+      <CandidatesList candidates={candidates} />
     </div>
   );
 }
